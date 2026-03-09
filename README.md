d15ebb3ff329e193436dc5bc546cbc76aca1488e
Sarveen3 <manpreetkaur53131@gmail.com>
2026-03-10 01:54:16 +0530

    my commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Journal REST API - Spring Boot
+
+This project is a REST API built using Spring Boot.
+
+## Features
+- Create journal entry
+- Get all journal entries
+- Update journal entry
+- Delete journal entry
+
+## Technologies Used
+- Java
+- Spring Boot
+- Gradle
+- REST API
+- Postman
+
+## Endpoints
+
+GET /journal  
+POST /journal  
+PUT /journal/{id}  
+DELETE /journal/{id}
+
+## Author
+Sarveen
---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,59 @@
+package net.engineeringdigest.journalApp.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+
+
+
+
+
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        //localhost:8080/journal GET
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myAbcEntry){
+        //localhost:8080/journal POST;
+        journalEntries.put(myAbcEntry.getId(), myAbcEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+    
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+    
+    @PutMapping("id/{id}")
+    public JournalEntry updateJournalById(@PathVariable Long id, @RequestBody JournalEntry myAbcEntry){
+        return journalEntries.put(id, myAbcEntry);
+    }   
+    
+    
+}
